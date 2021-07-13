8e51da87354eb2eb0149ec7302b0d43a69fa5fbe
CKanishka <ckanishka24@gmail.com>
2021-07-13 14:01:40 +0530

    Map default location moved to Constants file

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import {Marker,GoogleApiWrapper} from 'google-maps-react';
-import GoogleMap from 'google-maps-react';
+import React from "react";
+import { Marker, GoogleApiWrapper } from "google-maps-react";
+import GoogleMap from "google-maps-react";
+import { DEFAULT_LOCATION } from "../Constants";
 
 class MapContainer extends React.Component {
-  
-    onMarkerDragEnd = (coord) => {
-      if(!this.props.dragMarkerDisable){
-      this.props.onMarkerDragEnd(coord)
-      }
-    };
-  
-    render() {
-     const lat = 22.0127399;
-     const lng = 71.4523598;
-     const {location,draggable} = this.props
-      return (
-        <div style={{position:"relative",overflow:"hidden",height:"200px"}}>
-        <div id="mapcontainer" >
-        <GoogleMap 
-                google={this.props.google}
-                initialCenter = {{lat,lng} }
-                center = {{lat,lng}}
-                style={{
-                width: "100%",
-                height: "100%",
-                }}
-                zoom={3}
-            >
+  onMarkerDragEnd = (coord) => {
+    if (!this.props.dragMarkerDisable) {
+      this.props.onMarkerDragEnd(coord);
+    }
+  };
+
+  render() {
+    const { location, draggable, google } = this.props;
+    return (
+      <div
+        style={{ position: "relative", overflow: "hidden", height: "200px" }}
+      >
+        <div id="mapcontainer">
+          <GoogleMap
+            google={google}
+            initialCenter={DEFAULT_LOCATION}
+            center={DEFAULT_LOCATION}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            zoom={3}
+          >
             <Marker
               position={location}
               draggable={draggable}
               onDragend={(t, map, coord) => this.onMarkerDragEnd(coord)}
               name={"Marker 1"}
             />
-        </GoogleMap>
+          </GoogleMap>
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyCW5oLWMlu_ZeqE10XtrpyuFutbvkduEwA'
-  }) (MapContainer);  
+  apiKey: "AIzaSyCW5oLWMlu_ZeqE10XtrpyuFutbvkduEwA",
+})(MapContainer);
